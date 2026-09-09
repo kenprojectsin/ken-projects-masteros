3130535613e6720e1a3321b7d023f6ff71869cc7
kenprojectsin <kenprojectsin@gmail.com>
2026-09-09 12:32:14 +0530

    43

--- beta/sw.js
+++ beta/sw.js
@@ -18,7 +18,7 @@
 // a changed CACHE_NAME means the browser sees this as a genuinely different
 // service worker file, triggering the install/updatefound/waiting flow that
 // index.html's updatefound listener picks up and turns into the orange banner.
-const CACHE_NAME = 'ken-traders-beta-vbeta42';
+const CACHE_NAME = 'ken-traders-beta-vbeta43';
 const SHELL_FILES = [
   './index.html',
   './manifest.json',
